f31b8c1f1e4ef17a5aad73620e12d2cdb5b08f5f
lushi5316 <44177971+lushi5316@users.noreply.github.com>
2018-11-23 02:00:05 -0500

    Add files via upload

--- CScraper.py
+++ CScraper.py
@@ -40,10 +40,10 @@ class CScraper():
             self.driver.get(page)
             wait = WebDriverWait(self.driver, self.delay)
             wait.until(EC.presence_of_element_located((By.ID, "rendered-content")))
-            print(page[:6] + "Page is ready")
+            print(page + "\n" + "    Page is ready")
             loaded = True
         except TimeoutException:
-            print("Loading took too much time")
+            print(page, "\n" + "Loading took too much time")
         return loaded
 # given the base url and the page range generate a list of url string
 # input: minPage, int, the minimum page number; maxPage, int, the minimum page number
@@ -83,14 +83,16 @@ class CScraper():
     def extract_post_information(self, pageReady,url):
         if pageReady == True:
             try:
+                infoExtracted = False
                 wait = WebDriverWait(self.driver, self.delay)
                 wait.until(EC.presence_of_element_located((By.CLASS_NAME, "forReader")))
                 all_posts = self.driver.find_elements_by_class_name("rc-ThreadsListEntry")
-                print(type(all_posts))
-                print("the length of all_posts is: ", len(all_posts))
-                print(all_posts[0])
+                #print(type(all_posts))
+                #print("the length of all_posts is: ", len(all_posts))
+                #print(all_posts[0])
                 if len(all_posts) != 0:
                     infoExtracted = True
+                    print("Info has been Extracted")
                 postList = []
                 allposttxt = open("allpost.txt", "a+", encoding="utf8")
                 postcounter = 0
@@ -100,26 +102,32 @@ class CScraper():
                     allposttxt.write(info + "\n" + "\n")
                     info = info.split('\n')
                     title = info[0]
-                    print(title)
+                    #print(title)
                     line2 = info[1]
-                    print(line2)
+                    #print(line2)
                     line2Map = self.__read_line2(line2)
-                    print(type(line2Map.items()))
+                    #print(type(line2Map.items()))
                     views = info[2]
                     replies = info[4]
                     postMap = {"postNum": postcounter, "title": title, "views": views, "replies": replies}
-                    print(postMap.items())
+                    #print(postMap.items())
                     for item in line2Map.keys():
                         postMap[item] = line2Map[item]
-                        print("postMap line2Map transfer good")
-                    print(["title"], postMap["views"], postMap["replies"])
+                        #print("postMap line2Map transfer good")
+                    #print(["title"], postMap["views"], postMap["replies"])
                     postList.append(postMap)
-                    print(len(postList))
+                    #print(len(postList))
                 allposttxt.close()
                 self.postBlueMap[url] = postList
+                print("Info has been recorded properly")
             except:
-                print("cannot fine the posts")
+                print("The post is not recorded properly.")
 
+
+# A private method that breaks down the second line of the raw post information
+# input: a, a string, the line that needs to be parsed
+# return: a dictionary that contains the information for 1) if the post is highlighted 2) if the post contains
+        # a staff reply 3) by whom the post is created by 4) by whom the post is relied
     def __read_line2(self, a):
         line2 = {}
         if "Highlighted" in a:
@@ -136,16 +144,18 @@ class CScraper():
             namePostSplit = timeSplit[0].split("Last post by ")
             line2["nameLastPosted"] = namePostSplit[-1]
             line2["nameCreated"] = "NA"
-            print(line2)
+            #print(line2)
         if "Created by" in a:
             timeSplit = a.split("· ")
             line2["timePosted"] = timeSplit[-1]
             namePostSplit = timeSplit[0].split("Created by")
             line2["nameCreated"] = namePostSplit[-1]
             line2["nameLastPosted"] = "NA"
-        print(line2)
-        print(type(line2))
+        #print(line2)
+        #print(type(line2))
         return line2
+
+
 # write all the post information from the big dictionary in to a CSV file named "post_title.csv"
 # input: none
 # return: void
@@ -168,27 +178,26 @@ class CScraper():
                         except:
                             continue
         except:
-            print("CSV write in fail")
+            print("CSV write-in fail")
 
 
 if __name__ == '__main__':
-    email = "lushi@umich.edu"
-    password = "MEI1blossom"
+    email = ""
+    password = ""
     course = "python-data-analysis"
     week = "1"
     f = CScraper(course, week)
     baseUrlLoaded = f.load_one_url(f.baseurl)
     loginSucess = f.login(email, password)
-    print(loginSucess)
+    print("The page is loged in successfully: ", loginSucess)
     if loginSucess:
-        urlList = f.url_generator(1, 15)
+        urlList = f.url_generator()
         for eachUrl in urlList:
             pageLoaded = f.load_one_url(page=eachUrl)
             f.extract_post_information(pageLoaded, eachUrl)
     bigmap = f.bigMap()
     for item in bigmap.keys():
-        print("---------Printing the bigMap---------------")
-        print(bigmap.keys())
+        print("---------Printing from the bigMap---------------")
         print(bigmap[item])
     f.write_csv()
     f.quit()
